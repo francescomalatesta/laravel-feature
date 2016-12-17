a42b63fb168dba13f8a0277f7bf5e198c02f5235
Francesco Malatesta <hellofrancesco@gmail.com>
2016-12-17 14:05:39 +0100

    Add basic TestCase for Laravel implementation

---
+++ tests/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LaravelFeature\Tests;
+
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom([
+            '--database' => 'testing',
+            '--realpath' => realpath(__DIR__.'/../src/Migrations'),
+        ]);
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
